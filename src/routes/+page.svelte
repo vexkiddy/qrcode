2f56b504d0539920bc38445259f3c814a9393ac1
Timmy <generic@mrtim.co>
2022-09-15 17:15:34 +0200

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div id="canvas"></div>
+I cant get this QR CODE to work 
+<script>
+   
+    import QRCodeStyling from "qr-code-styling";
+
+    import { onMount } from 'svelte';
+    onMount(async () => {
+        const qrCode = new QRCodeStyling({
+            width: 300,
+            height: 300,
+            image:
+            "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+                dotsOptions: {
+                color: "#4267b2",
+                type: "rounded"
+            },
+            imageOptions: {
+                crossOrigin: "anonymous",
+                margin: 20
+            }
+        });
+        qrCode.append(document.getElementById("canvas"));
+        qrCode.download({ name: "qr", extension: "svg" });
+    });
+</script>
+
+
